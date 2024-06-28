54ffc755662a24617193af81b85625ddd2e2cfbe
SundayMba <sundinoh@gmail.com>
2024-06-28 11:52:34 +0100

    created one greeting endpoint

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Internship - HNG 
+
+# USAGE
+ ## Create a virtaul environment
+    `python3 -m venv .venv`
+## Activate the virtual Environment
+    `source .venv/bin/activate`
+## install the necessary packages in the requirement file
+    `pip install -r requirements.txt`
---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+
+@app.route("/api/hello")
+def greeting():
+    visitor_name = request.args.get("visitor_name", default='Guest', type=str)
+    response = {
+        "client_ip": request.remote_addr,
+        "greeting": "Hello, {}!".format(visitor_name.strip('"'))
+    }
+    return jsonify(response)
