d4ac8ebf0da11b49a7506364bec0cabb822b4b4a
SundayMba <sundinoh@gmail.com>
2024-06-30 22:04:47 +0100

    added location info

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import requests
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -14,7 +15,8 @@ def greeting():
     visitor_name = request.args.get("visitor_name", default='Guest', type=str)
     response = {
         "client_ip": client_ip,
-        "greeting": "Hello, {}!".format(visitor_name.strip('"'))
+        "location": get_client_city(client_ip),
+        "greeting": "Hello, {}!, the temperature is 11 degrees celcius in new York.".format(visitor_name.strip('"'))
     }
     return jsonify(response)
 
@@ -23,3 +25,15 @@ def index():
     return jsonify({
         "Message": "Welcome to Home Page"
     })
+
+def get_client_city(ip_address):
+    url = "https://ipinfo.io"
+    params = {
+        "token": "6a11f15ed06812"
+    }
+    try:
+        response = requests.get(url, params).json()
+        city = response.get("city")
+    except:
+        city = "california"
+    return city
